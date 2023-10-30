c990a1a71258ef14a2ed43061dada57eb9319446
Adeeb Akhtar <adeebsamea@gmail.com>
2023-10-30 20:47:16 +0530

    Directives

--- hotelinventoryapp/src/app/rooms/rooms.component.ts
+++ hotelinventoryapp/src/app/rooms/rooms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Room,RoomList} from './rooms'
 
 @Component({
   selector: 'hinv-rooms',
@@ -10,6 +11,42 @@ export class RoomsComponent implements OnInit {
   hotelName = "Hilton Hotel";
   numberOfRooms = '10';
   hideRooms = false
+  
+  rooms : Room = {
+    availableRooms: 10,
+    bookedRooms: 5,
+    totalRooms: 20
+  }
+
+  roomList : RoomList[] = [
+    {
+      roomNumber : 1,
+      roomType : "Deluxe",
+      amenities : 'AC, Wifi, TV,',
+      price : 1000,
+      photos : 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131',
+      checkInTime : new Date('11-Nov-2011'),
+      checkOutTime : new Date('13-Nov-2011')
+    },
+    {
+      roomNumber : 2,
+      roomType : "Private suite",
+      amenities : 'AC, Wifi, TV, Jacuzzi, Food',
+      price : 1500,
+      photos : 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131',
+      checkInTime : new Date('12-Nov-2011'),
+      checkOutTime : new Date('15-Nov-2011')
+    },
+    {
+      roomNumber : 3,
+      roomType : "Standard",
+      amenities : 'Wifi, TV,',
+      price : 500,
+      photos : 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131',
+      checkInTime : new Date('12-Nov-2011'),
+      checkOutTime : new Date('15-Nov-2011')
+    }
+  ]
 
   constructor() { }
 
